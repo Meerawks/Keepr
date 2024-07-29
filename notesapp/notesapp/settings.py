31a86f57cff63676b85777ac3c08d7fd4f80cb0e
Sameer Asif <sameer.asif.mughal@gmail.com>
2024-07-29 20:32:34 +0500

    updated

--- notesapp/notesapp/settings.py
+++ notesapp/notesapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5fbji#6w1l8b)#5@k1-9i&4e17tbobynb4^&v%u^*+i)b!&ht
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,9 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
